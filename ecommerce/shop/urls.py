75da7e2d97d4d451df1e68de21db634c14f2634d
Avp123456 <akankshapawar8866@gmail.com>
2025-04-18 17:41:13 +0530

    navbar and categories

--- ecommerce/shop/urls.py
+++ ecommerce/shop/urls.py
@@ -5,6 +5,11 @@ from .views import signup_view
 app_name = 'shop'
 
 urlpatterns = [
+
+
+
+    path('base/', views.base_view, name='base'),
+
     # User-related
     path('signup/', signup_view, name='signup'),
 
@@ -19,4 +24,5 @@ urlpatterns = [
     path('', views.product_list, name='product_list'),
     path('<slug:category_slug>/', views.product_list, name='product_list_by_category'),
     path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
+
 ]
